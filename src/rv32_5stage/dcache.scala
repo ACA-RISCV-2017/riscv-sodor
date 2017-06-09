3d5fc20b15f447bb8db215f7e09737c41f723f46
Chiang, Yi-Yo@home <b03902082@ntu.edu.tw>
2017-06-10 02:08:35 +0800

    write-through cache

--- src/rv32_5stage/dcache.scala
+++ src/rv32_5stage/dcache.scala
@@ -11,15 +11,279 @@ package object AcaCustom
 			val core_port = (new MemPortIo(data_width=conf.xprlen)).flip
 			val mem_port = new MemPortIo(data_width=conf.xprlen)
 		}
+
+                io.mem_port.req.valid := Bool(false)
+                io.mem_port.req.bits.addr := Bits(0)
+                io.mem_port.req.bits.data := Bits(0)
+                io.mem_port.req.bits.fcn := M_XRD
+                io.mem_port.req.bits.typ := MT_WU
+                io.core_port.resp.valid := Bool(false)
+                io.core_port.resp.bits.data := Bits(0)
+                io.core_port.req.ready := Bool(true)
+
+                val num_bytes_per_cache_line = 4
+                val cache_idx_width = 10
+
+                val num_words_per_cache_line = num_bytes_per_cache_line / 4
+                val num_cache_lines = 1 << cache_idx_width
+                val idx_lsb = log2Up(num_bytes_per_cache_line) // 2
+                val block_width = 8 * num_bytes_per_cache_line
+                val tag_width = conf.xprlen - idx_lsb - cache_idx_width
+                val flag_width = 2 // valid and dirty bit
+                val num_bits_per_cache_line = flag_width + tag_width + block_width
+                val data_bank = Mem(Bits(width = num_bits_per_cache_line),
+                                    num_cache_lines,
+                                    seqRead = false
+                                    )
+
+                val req_valid      = io.core_port.req.valid
+                val req_addr       = io.core_port.req.bits.addr
+                val req_data       = io.core_port.req.bits.data
+                val req_fcn        = io.core_port.req.bits.fcn
+                val req_typ        = io.core_port.req.bits.typ
+                val byte_shift_amt = io.core_port.req.bits.addr(idx_lsb-1, 0)
+                val bit_shift_amt  = Cat(byte_shift_amt, UInt(0, 3))
+
+                val data_idx = req_addr >> idx_lsb
+                val cache_idx = data_idx(cache_idx_width-1, 0)
+                val tag_idx = (data_idx >> cache_idx_width)(tag_width-1, 0)
+                // addr = [tag_idx, cache_idx, byte_shift_amt]
+
+                val cache_not_init = Reg(init = Bool(true))
+                when (cache_not_init) {
+                  cache_not_init := Bool(false)
+                  for (i <- 0 until num_cache_lines) {
+                    data_bank(i) := Bits(0)
+                  }
+                }
+
+                val line = data_bank(cache_idx)
+                val block = line(block_width-1, 0)
+                val tag = (line >> block_width)(tag_width-1, 0)
+                val flag = (line >> (block_width + tag_width))(flag_width-1, 0)
+                val dirty_bit = Bool(flag(0))
+                val valid_bit = Bool(flag(1))
+
+                // write-back cache
+                when (req_valid && req_fcn === M_XWR) {
+                  // write access
+                  when (valid_bit && tag === tag_idx) {
+                    // cache hit
+                    val wdata = Cat(
+                      Bits("b11", 2), // set valid bit and dirty bit
+                      Bits(0, tag_width),
+                      Fill(num_words_per_cache_line, StoreDataGen(req_data, req_typ))
+                    )
+                    val wmask = Cat(
+                      Bits("b11", 2),
+                      Bits(0, tag_width),
+                      (StoreMask(req_typ) << bit_shift_amt)(block_width-1, 0)
+                    )
+                    data_bank.write(cache_idx, wdata, wmask)
+                    // io.core_port.resp.bits.data := req_data
+                    io.core_port.resp.valid := Bool(true)
+                    io.core_port.req.ready := Bool(true)
+
+                    /* FIXME */
+                    // Something is very wrong, write-back cache will exception
+                    // write-through cache seems okay. test `fence.i` always crash
+                    // =(
+                    io.core_port.resp.valid := Bool(false)
+                    io.core_port.req.ready := Bool(false)
+                    val new_block = data_bank(cache_idx)
+                    // Need to write-back dirty data
+                    val s_ready :: s_waiting :: Nil = Enum(UInt(), 2)
+                    val state = Reg(init = s_ready)
+                    switch (state) {
+                      is (s_ready) {
+                        io.mem_port.req.valid := Bool(true)
+                        io.mem_port.req.bits.addr := Cat(tag, cache_idx, Bits(0, 2))
+                        io.mem_port.req.bits.data := new_block
+                        io.mem_port.req.bits.fcn := M_XWR
+                        io.mem_port.req.bits.typ := MT_WU
+                        state := s_waiting
+                      }
+                      is (s_waiting) {
+                        when (io.mem_port.resp.valid) {
+                          io.mem_port.req.valid := Bool(false)
+                          data_bank(cache_idx) := Bits(0)
+                          io.core_port.resp.valid := Bool(true)
+                          io.core_port.req.ready := Bool(true)
+                          state := s_ready
+                        } .otherwise {
+                          io.mem_port.req.valid := Bool(true)
+                          io.mem_port.req.bits.addr := Cat(tag, cache_idx, Bits(0, 2))
+                          io.mem_port.req.bits.data := new_block
+                          io.mem_port.req.bits.fcn := M_XWR
+                          io.mem_port.req.bits.typ := MT_WU
+                        }
+                      }
+                    }
+
+
+                  } .otherwise {
+                    // cache miss
+                    io.core_port.req.ready := Bool(false)
+                    when (valid_bit && dirty_bit) {
+                      // Need to write-back dirty data first
+                      val s_ready :: s_waiting :: Nil = Enum(UInt(), 2)
+                      val state = Reg(init = s_ready)
+                      switch (state) {
+                        is (s_ready) {
+                          io.mem_port.req.valid := Bool(true)
+                          io.mem_port.req.bits.addr := Cat(tag, cache_idx, Bits(0, 2))
+                          io.mem_port.req.bits.data := block
+                          io.mem_port.req.bits.fcn := M_XWR
+                          io.mem_port.req.bits.typ := MT_WU
+                          state := s_waiting
+                        }
+                        is (s_waiting) {
+                          when (io.mem_port.resp.valid) {
+                            io.mem_port.req.valid := Bool(false)
+                            data_bank(cache_idx) := Bits(0)
+                            state := s_ready
+                          } .otherwise {
+                            io.mem_port.req.valid := Bool(true)
+                            io.mem_port.req.bits.addr := Cat(tag, cache_idx, Bits(0, 2))
+                            io.mem_port.req.bits.data := block
+                            io.mem_port.req.bits.fcn := M_XWR
+                            io.mem_port.req.bits.typ := MT_WU
+                          }
+                        }
+                      }
+                    } .otherwise {
+                      val s_ready :: s_waiting :: Nil = Enum(UInt(), 2)
+                      val state = Reg(init = s_ready)
+                      switch (state) {
+                        is (s_ready) {
+                          io.mem_port.req.valid := Bool(true)
+                          io.mem_port.req.bits.addr := Cat(req_addr >> 2, Bits(0, 2))
+                          io.mem_port.req.bits.fcn := M_XRD
+                          io.mem_port.req.bits.typ := MT_WU
+                          state := s_waiting
+                        }
+                        is (s_waiting) {
+                          when (io.mem_port.resp.valid) {
+                            io.mem_port.req.valid := Bool(false)
+                            io.mem_port.req.bits.addr := Cat(req_addr >> 2, Bits(0, 2))
+                            io.mem_port.req.bits.fcn := M_XRD
+                            io.mem_port.req.bits.typ := MT_WU
+                            val orig_data = io.mem_port.resp.bits.data
+                            val wdata = Fill(num_words_per_cache_line, StoreDataGen(req_data, req_typ))
+                            val wmask = (StoreMask(req_typ) << bit_shift_amt)(block_width-1, 0)
+                            data_bank(cache_idx) := Cat(
+                              Bits("b11", 2),
+                              tag_idx,
+                              (orig_data & ~wmask) | (wdata & wmask)
+                            )
+                            // io.core_port.resp.bits.data := req_data
+                            io.core_port.resp.valid := Bool(true)
+                            io.core_port.req.ready := Bool(true)
+                            state := s_ready
+                          } .otherwise {
+                            io.mem_port.req.valid := Bool(true)
+                            io.mem_port.req.bits.addr := Cat(req_addr >> 2, Bits(0, 2))
+                            io.mem_port.req.bits.fcn := M_XRD
+                            io.mem_port.req.bits.typ := MT_WU
+                          }
+                        }
+                      }
+                    }
+                  }
+                } .elsewhen (req_valid && req_fcn === M_XRD) {
+                  // read access
+                  when (valid_bit && tag === tag_idx) {
+                    // cache hit
+                    io.core_port.resp.bits.data := LoadDataGen(block >> bit_shift_amt, req_typ)
+                    io.core_port.resp.valid := Bool(true)
+                    io.core_port.req.ready := Bool(true)
+                  } .otherwise {
+                    // cache miss
+                    io.core_port.req.ready := Bool(false)
+                    when (valid_bit && dirty_bit) {
+                      // Need to write-back dirty data first
+                      val s_ready :: s_waiting :: Nil = Enum(UInt(), 2)
+                      val state = Reg(init = s_ready)
+                      switch (state) {
+                        is (s_ready) {
+                          io.mem_port.req.valid := Bool(true)
+                          io.mem_port.req.bits.addr := Cat(tag, cache_idx, Bits(0, 2))
+                          io.mem_port.req.bits.data := block
+                          io.mem_port.req.bits.fcn := M_XWR
+                          io.mem_port.req.bits.typ := MT_WU
+                          state := s_waiting
+                        }
+                        is (s_waiting) {
+                          when (io.mem_port.resp.valid) {
+                            io.mem_port.req.valid := Bool(false)
+                            data_bank(cache_idx) := Bits(0)
+                            state := s_ready
+                          } .otherwise {
+                            io.mem_port.req.valid := Bool(true)
+                            io.mem_port.req.bits.addr := Cat(tag, cache_idx, Bits(0, 2))
+                            io.mem_port.req.bits.data := block
+                            io.mem_port.req.bits.fcn := M_XWR
+                            io.mem_port.req.bits.typ := MT_WU
+                          }
+                        }
+                      }
+                    } .otherwise {
+                      val s_ready :: s_waiting :: Nil = Enum(UInt(), 2)
+                      val state = Reg(init = s_ready)
+                      switch (state) {
+                        is (s_ready) {
+                          when (io.mem_port.req.ready) {
+                            io.mem_port.req.valid := Bool(true)
+                            io.mem_port.req.bits.addr := Cat(req_addr >> 2, Bits(0, 2))
+                            io.mem_port.req.bits.fcn := M_XRD
+                            io.mem_port.req.bits.typ := MT_WU
+                            state := s_waiting
+                          }
+                        }
+                        is (s_waiting) {
+                          when (io.mem_port.resp.valid) {
+                            val orig_data = io.mem_port.resp.bits.data
+                            io.mem_port.req.valid := Bool(false)
+                            io.mem_port.req.bits.addr := Cat(req_addr >> 2, Bits(0, 2))
+                            io.mem_port.req.bits.fcn := M_XRD
+                            io.mem_port.req.bits.typ := MT_WU
+                            data_bank(cache_idx) := Cat(
+                              Bits("b10", 2),
+                              tag_idx,
+                              orig_data
+                            )
+                            io.core_port.resp.bits.data := LoadDataGen(orig_data >> bit_shift_amt, req_typ)
+                            io.core_port.resp.valid := Bool(true)
+                            io.core_port.req.ready := Bool(true)
+                            state := s_ready
+                          } .otherwise {
+                            io.mem_port.req.valid := Bool(true)
+                            io.mem_port.req.bits.addr := Cat(req_addr >> 2, Bits(0, 2))
+                            io.mem_port.req.bits.fcn := M_XRD
+                            io.mem_port.req.bits.typ := MT_WU
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
 	}
 
-	class NoDCache()(implicit conf: SodorConfiguration) extends DCacheInterface
+	class NoDCache()(implicit conf: SodorConfiguration) extends Module
 	{
+		val io = new Bundle {
+			val core_port = (new MemPortIo(data_width=conf.xprlen)).flip
+			val mem_port = new MemPortIo(data_width=conf.xprlen)
+		}
 		io.mem_port <> io.core_port
 	}
 
-	class NoDCache2()(implicit conf: SodorConfiguration) extends DCacheInterface
+	class NoDCache2()(implicit conf: SodorConfiguration) extends Module
 	{
+		val io = new Bundle {
+			val core_port = (new MemPortIo(data_width=conf.xprlen)).flip
+			val mem_port = new MemPortIo(data_width=conf.xprlen)
+		}
 		// Extract desired data from burst
 		val burst_len = 64 // 64 byte per line
 		val burst_len_bit = 6 // 2^6 = 64
@@ -50,20 +314,5 @@ package object AcaCustom
 		
 	}
 
-	type DCache = NoDCache2
-
-	//appropriately mask and sign-extend data for the core                  
-	object LoadDataGen                                                      
-	{                                                                       
-	   def apply(data: Bits, typ: Bits) : Bits =                            
-	   {                                                                    
-		  val out = Mux(typ === MT_H,  Cat(Fill(16, data(15)),  data(15,0)),
-					Mux(typ === MT_HU, Cat(Fill(16, UInt(0x0)), data(15,0)),
-					Mux(typ === MT_B,  Cat(Fill(24, data(7)),    data(7,0)),
-					Mux(typ === MT_BU, Cat(Fill(24, UInt(0x0)), data(7,0)), 
-										data(31,0)))))                      
-																			
-		  return out                                                        
-	   }                                                                    
-	}
+        type DCache = DCacheInterface
 }
