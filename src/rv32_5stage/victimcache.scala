00338f123c2ff169ee658adc3e471cb7c0245afb
Chi-chung Chen <bryan830401@gmail.com>
2017-06-22 11:15:17 -0700

    Victim now support multiple entries. (Default: 4)

--- src/rv32_5stage/victimcache.scala
+++ src/rv32_5stage/victimcache.scala
@@ -12,23 +12,30 @@ package object AcaCustomVictimCache
       val mem_port = new MemPortIo(data_width=conf.xprlen)
     }
 
-    val cache_valid = Reg(Bool(), init=Bool(false))
-    val cache_addr = Reg(Bits(width = conf.xprlen))
-    val cache_data = Vec.fill(16) { Reg(Bits(width=32)) }
+	  val entry_num = 4
+
+    val cache_valid = Vec.fill(entry_num) { Reg(Bool(), init=Bool(false)) }
+    val cache_addr = Vec.fill(entry_num) { Reg(Bits(width = conf.xprlen)) }
+    val cache_data = Vec.fill(entry_num) { Vec.fill(16) { Reg(Bits(width=32)) } }
 
     val s_idle :: s_hit :: Nil = Enum(UInt(), 2)
     val state = Reg(init = s_idle)
 
     val reg_req = Reg(io.cache_port.req)
+    val hit_index = Reg(Bits(width=log2Up(entry_num)))
+    val write_index = Reg(Bits(width=log2Up(entry_num)), init=Bits(0))
 
     // state
     switch (state) {
       is(s_idle) {
-        when (io.cache_port.req.valid && io.cache_port.req.bits.fcn === M_XRD && 
-          cache_valid && aligned(io.cache_port.req.bits.addr) === cache_addr)
+        when (io.cache_port.req.valid && io.cache_port.req.bits.fcn === M_XRD)
         {
-          printf("Hit!")
-          state := s_hit
+          for (i <- 0 until entry_num) {
+            when (cache_valid(i) && aligned(io.cache_port.req.bits.addr) === cache_addr(i)) {
+              printf("Victim Cache Hit!")
+              state := s_hit
+            }
+          }
         }
       }
       is(s_hit) {
@@ -46,29 +53,37 @@ package object AcaCustomVictimCache
 
     switch (state) {
       is(s_idle) {
-        when (io.cache_port.req.valid && io.cache_port.req.bits.fcn === M_XRD &&
-          cache_valid && aligned(io.cache_port.req.bits.addr) === cache_addr)
+        when (io.cache_port.req.valid && io.cache_port.req.bits.fcn === M_XRD)
         {
-          io.cache_port.resp.valid := Bool(false)
-          io.mem_port.req.valid := Bool(false)
+          for (i <- 0 until entry_num) {
+            when (cache_valid(i) && aligned(io.cache_port.req.bits.addr) === cache_addr(i)) {
+              io.cache_port.resp.valid := Bool(false)
+              io.mem_port.req.valid := Bool(false)
+              hit_index := Bits(i)
+            }
+          }
         }
       }
       is(s_hit) {
         io.cache_port.req.ready := Bool(false)
         io.cache_port.resp.valid := Bool(true)
-        io.cache_port.resp.bits.data := cache_data(UInt(reg_req.bits.addr(5, 2)))
-        io.cache_port.resp.bits.burst_data := cache_data
+        io.cache_port.resp.bits.data := cache_data(UInt(hit_index))(UInt(reg_req.bits.addr(5, 2)))
+        io.cache_port.resp.bits.burst_data := cache_data(UInt(hit_index))
         io.mem_port.req.valid := Bool(false)
       }
     }
 
     // Victim Cache
-    when (state === s_idle && io.cache_port.req.valid && io.cache_port.req.bits.fcn === M_XWRBURST)
-    {
-      printf("Changed!")
-      cache_valid := Bool(true)
-      cache_addr := aligned(io.cache_port.req.bits.addr)
-      cache_data := io.cache_port.req.bits.burst_data
+    when (state === s_idle && io.cache_port.req.valid && io.cache_port.req.bits.fcn === M_XWRBURST) {
+      for (i <- 0 until entry_num) {
+        when (cache_valid(i) && aligned(io.cache_port.req.bits.addr) === cache_addr(i) && write_index =/= Bits(i)) {
+          cache_valid(i) := Bool(false)
+        }
+      }
+      cache_valid(UInt(write_index)) := Bool(true)
+      cache_addr(UInt(write_index)) := aligned(io.cache_port.req.bits.addr)
+      cache_data(UInt(write_index)) := io.cache_port.req.bits.burst_data
+      write_index := write_index + UInt(1)
     }
   }
 
